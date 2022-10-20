31267b0aacfda065d35b60336d06ba331e1f5a48
Suji Falcon <suji.falcon@gmail.com>
2022-10-20 17:39:07 -0400

    cleaned up extra code and files that i didn't need anymore

--- models/Character.js
+++ models/Character.js
@@ -1,22 +1,6 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-// const Character = new Schema({
-//   name: String,
-//   nicknames: [{ type: String }],
-//   birthplace: String,
-//   birthday: String,
-//   family: [{ type: String }],
-//   romances: [{ type: String }],
-//   friends: [{ type: String }],
-//   enemies: [{ type: String }],
-//   alive: Boolean,
-//   portrayedBy: String,
-//   quotes: [{ type: String }],
-//   hobbies: [{ type: String }],
-//   trivia: [{ type: String }],
-// });
-
 const Character = new Schema({
   name: { type: String },
   title: { type: String },

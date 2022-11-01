c3a21ce8d68ff50c9af7803ba5f58b17442640ec
Suji Falcon <suji.falcon@gmail.com>
2022-11-01 14:39:00 -0400

    work on this later

--- seed/data.js
+++ seed/data.js
@@ -4,46 +4,49 @@ import Character from '../models/Character.js';
 // import characters from './characters.json' assert { type: 'json' };
 
 const insertData = async (characters) => {
-  let charactersData = [];
+	let charactersData = [];
 
-  for (let char in characters) {
-    charactersData.push({
-      name: char,
-      title: characters[char].title,
-      blurb: characters[char].blurb,
-      image_loading: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${char}_0.jpg`,
-      image_splash: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${char}_0.jpg`,
-      tags: characters[char].tags,
-      partype: characters[char].partype,
-    });
-  }
+	for (let char in characters) {
+		charactersData.push({
+			name: char,
+			title: characters[char].title,
+			blurb: characters[char].blurb,
+			image_loading: `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${char}_0.jpg`,
+			image_splash: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${char}_0.jpg`,
+			tags: characters[char].tags,
+			partype: characters[char].partype,
+		});
+	}
 
-  // reset database
-  await db.dropDatabase();
-  // insert data
-  await Character.create(charactersData);
-  // close db connection
-  await db.close();
+	// reset database
+	await db.dropDatabase();
+	// insert data
+	await Character.create(charactersData);
+	// close db connection
+	await db.close();
 };
 
 const fetchCharacters = async () => {
-  let characters = await axios.get(
-    'http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json',
-  );
-  // ^ That request gets all the champion names
-  // For each champion name, you now need to fire another request.
-  const promiseArr = [];
-  for each character {
-    promiseArr.push(
-      axios.get(`http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion/${champName}`)
-    )
-  }
-  // Now you have all the get request promises in an array, just await all
-  const arrOfChampResponses = await Promise.all(promiseArr);
-  // console.log(characters.data.data);
-  
-
-  insertData(characters.data.data);
+	let characters = await axios.get(
+		'http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json',
+	);
+	console.log(characters.data.data['Aatrox']);
 };
+// ^ That request gets all the champion names
+// For each champion name, you now need to fire another request.
+const promiseArr = [];
+characters.forEach((character) => {
+	promiseArr.push(
+		axios.get(
+			`http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion/${champName}`,
+		),
+	);
+});
+// Now you have all the get request promises in an array, just await all
+// const arrOfChampResponses = await Promise.all(promiseArr);
+// console.log(characters.data.data);
+
+insertData(characters.data.data);
+// };
 
 fetchCharacters();

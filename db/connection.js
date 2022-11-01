19579c64ee4c3025a4ab430d0b6f0246a5fa7c79
Suji Falcon <suji.falcon@gmail.com>
2022-11-01 14:45:30 -0400

    removed pseudocode

--- db/connection.js
+++ db/connection.js
@@ -3,18 +3,16 @@ import chalk from 'chalk';
 
 mongoose.set('returnOriginal', false);
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/express-harry-potter-api')
-  .catch((err) => {
-    console.log(`error connection go to mongodb: ${err.message}`);
-  });
+mongoose.connect('mongodb://127.0.0.1:27017/league-api').catch((err) => {
+	console.log(`error connection go to mongodb: ${err.message}`);
+});
 
 mongoose.connection.on('disconnected', () => {
-  console.log(chalk.bold('disconnected from mongodb'));
+	console.log(chalk.bold('disconnected from mongodb'));
 });
 
 mongoose.connection.on('error', (err) => {
-  console.log(chalk.red(`mongodb connection error: ${err}`));
+	console.log(chalk.red(`mongodb connection error: ${err}`));
 });
 
 export default mongoose.connection;
